97795d4b2ccc783be42644eeb362acb123e9923b
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-06-30 15:34:38 -0700

    Refactor API with underscore

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -18,14 +19,14 @@ app.get("/todos", function(req, res) {
 
 //GET /todos/:id (colon means that you can enter whatever id you want)
 app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id);
-    var matchedTodo;
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId}); //Underscore js!!
 
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    // todos.forEach(function(todo) {
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
 
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -36,7 +37,14 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, "description", "completed");
+
+    //THE POWER OF UNDERSCOREE
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
 
     body.id = todoNextId++;
 
