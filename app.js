51d4540ac1f6e0ea5e1fae66c1fb81aeaed19cae
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-06-30 15:53:20 -0700

    Add DELETE todos route

--- app.js
+++ app.js
@@ -35,6 +35,22 @@ app.get('/todos/:id', function(req, res) {
     }
 });
 
+app.delete('/todos/:id', function(req, res) {
+
+    var todoId = parseInt(req.params.id, 10);
+
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        //todos.splice(todos.indexOf(matchedTodo), 1);
+        res.json(matchedTodo);
+    } else {
+        res.status(404).json({"error": "no todo found with that id"});
+    }
+
+});
+
 // POST /todos
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, "description", "completed");
