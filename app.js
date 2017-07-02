8d69e49183cb476a2c9eb0488b6171eb7af84466
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-02 12:38:03 -0700

    Add PUT /todos/:id

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.get("/todos", function(req, res) {
 //GET /todos/:id (colon means that you can enter whatever id you want)
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId}); //Underscore js!!
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId
+    }); //Underscore js!!
 
     // todos.forEach(function(todo) {
     //     if (todoId === todo.id) {
@@ -35,20 +37,54 @@ app.get('/todos/:id', function(req, res) {
     }
 });
 
+// DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 
     var todoId = parseInt(req.params.id, 10);
 
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId
+    });
 
     if (matchedTodo) {
         todos = _.without(todos, matchedTodo);
         //todos.splice(todos.indexOf(matchedTodo), 1);
         res.json(matchedTodo);
     } else {
-        res.status(404).json({"error": "no todo found with that id"});
+        res.status(404).json({
+            "error": "no todo found with that id"
+        });
     }
 
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId
+    });
+    var body = _.pick(req.body, 'description', 'completed'); //Only captures the description and completed properties
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) { //Check is body has the "completed" attribute
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes); //Overrides all same attributes in matchedTodo with validAttributes
+    res.json(matchedTodo);
 });
 
 // POST /todos
