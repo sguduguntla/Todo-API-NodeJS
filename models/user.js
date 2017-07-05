04335355c855eff4d3793c04f757e6b4a76f6db6
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-05 12:43:50 -0700

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+var bcrypt = require('bcryptjs');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var User = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -8,20 +11,43 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [7, 100]
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
         hooks: {
             beforeValidate: function(user, options) {
                 //Changes email to lowercase version before email is validate for uniqueness
-                user.email = user.email.toLowerCase();
-
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
             }
         }
     });
+
+    User.prototype.toPublicJSON = function() {
+        var json = this.toJSON();
+        //makes sure only these are returned in the API
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+    }
+
+    return User;
 };
